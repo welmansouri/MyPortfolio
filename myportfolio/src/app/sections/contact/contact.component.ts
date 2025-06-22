b3bfd18b11a68c3555c2c36132494ba1dde2252c
wafae el mansouri <117817649+welmansouri@users.noreply.github.com>
2025-06-22 22:09:21 +0200

    contactPage

--- myportfolio/src/app/sections/contact/contact.component.ts
+++ myportfolio/src/app/sections/contact/contact.component.ts
@@ -1,11 +1,50 @@
-import { Component } from '@angular/core';
+import { Component ,} from '@angular/core';
+import {MatInputModule} from '@angular/material/input';
+import { CommonModule } from '@angular/common';
 
+import {
+  FormControl,
+  FormGroupDirective,
+  NgForm,
+  Validators,
+  FormsModule,
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+} from '@angular/forms';
+import {ErrorStateMatcher} from '@angular/material/core';
+import {MatFormFieldModule} from '@angular/material/form-field';
+export class MyErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+    const isSubmitted = form && form.submitted;
+    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
+  }
+}
 @Component({
   selector: 'app-contact',
-  imports: [],
+  imports: [MatInputModule,MatFormFieldModule,ReactiveFormsModule,CommonModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'
 })
 export class ContactComponent {
+    contactForm :FormGroup;
+
+    matcher = new MyErrorStateMatcher();
+
+    constructor(private fb: FormBuilder) {
+    this.contactForm = this.fb.group({
+      fullName: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      message: ['', Validators.required],
+    });
+  }
+
+  onSubmit(): void {
+    if (this.contactForm.valid) {
+      console.log(this.contactForm.value);
+    } else {
+      this.contactForm.markAllAsTouched();
+    }
+  }
 
 }
