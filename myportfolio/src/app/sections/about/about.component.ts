33719fcb9ce584249c315e35750d63294083d1e1
wafae el mansouri <117817649+welmansouri@users.noreply.github.com>
2025-09-28 23:25:17 +0200

    add responsive

--- myportfolio/src/app/sections/about/about.component.ts
+++ myportfolio/src/app/sections/about/about.component.ts
@@ -44,60 +44,60 @@ skills: Skill[] = [
   {
     name: "Programmation",
     techs: [
-      "/img/icons/java/java-original.svg",
-      "/img/icons/python/python-original.svg",
-      "/img/icons/javascript/javascript-original.svg",
-      "/img/icons/typescript/typescript-original.svg",
-      "/img/icons/mysql/mysql-original.svg" 
+      "/assets/img/icons/java/java-original.svg",
+      "/assets/img/icons/python/python-original.svg",
+      "/assets/img/icons/javascript/javascript-original.svg",
+      "/assets/img/icons/typescript/typescript-original.svg",
+      "/assets/img/icons/mysql/mysql-original.svg" 
     ]
   },
   {
     name: "Front-end & Mobile",
     techs: [
-      "/img/icons/angular/angular-original.svg",
-      "/img/icons/vuejs/vuejs-original.svg",
-      // "/img/icons/react/react-original.svg",    
-      "/img/icons/nextjs/nextjs-original.svg",
-      "/img/icons/redux/redux-original.svg",
-      "/img/icons/tailwindcss/tailwindcss-original.svg",
-      "/img/icons/vuetify/vuetify-original.svg"
+      "/assets/img/icons/angular/angular-original.svg",
+      "/assets/img/icons/vuejs/vuejs-original.svg",
+      // "/assets/img/icons/react/react-original.svg",    
+      "/assets/img/icons/nextjs/nextjs-original.svg",
+      "/assets/img/icons/redux/redux-original.svg",
+      "/assets/img/icons/tailwindcss/tailwindcss-original.svg",
+      "/assets/img/icons/vuetify/vuetify-original.svg"
     ]
   },
   {
     name: "Back-end & Bases de données",
     techs: [
-      "/img/icons/spring/spring-original.svg",
-      "/img/icons/hibernate/hibernate-original.svg",
-      "/img/icons/nodejs/nodejs-original.svg",
-      "/img/icons/postgresql/postgresql-original.svg",
-      "/img/icons/mysql/mysql-original.svg",
-      "/img/icons/mongodb/mongodb-original.svg",
-      "/img/icons/supabase/supabase-original.svg"
+      "/assets/img/icons/spring/spring-original.svg",
+      "/assets/img/icons/hibernate/hibernate-original.svg",
+      "/assets/img/icons/nodejs/nodejs-original.svg",
+      "/assets/img/icons/postgresql/postgresql-original.svg",
+      "/assets/img/icons/mysql/mysql-original.svg",
+      "/assets/img/icons/mongodb/mongodb-original.svg",
+      //"/assets/img/icons/supabase/supabase-original.svg"
     ]
   },
   {
     name: "DevOps, Cloud & Qualité",
     techs: [
-      "/img/icons/gitlab/gitlab-original.svg",
-      "/img/icons/kubernetes/kubernetes-plain.svg",
-      "/img/icons/jenkins/jenkins-original.svg",
-      "/img/icons/docker/docker-original.svg",
-      // "/img/icons/sonarqube/sonarqube.svg",          // custom si pas dans devicon
-      "/img/icons/maven/maven-original.svg",
-       "/img/icons/tomcat/tomcat-original.svg",          
+      "/assets/img/icons/gitlab/gitlab-original.svg",
+      "/assets/img/icons/kubernetes/kubernetes-plain.svg",
+      "/assets/img/icons/jenkins/jenkins-original.svg",
+      "/assets/img/icons/docker/docker-original.svg",
+      // "/assets/img/icons/sonarqube/sonarqube.svg",          // custom si pas dans devicon
+      "/assets/img/icons/maven/maven-original.svg",
+       "/assets/img/icons/tomcat/tomcat-original.svg",          
 
-      "/img/icons/gradle/gradle-original.svg",
-      "/img/icons/azure/azure-original.svg",
-      "/img/icons/junit/junit-original.svg",
-            "/img/icons/jupyter/jupyter-original.svg",
+      "/assets/img/icons/gradle/gradle-original.svg",
+      "/assets/img/icons/azure/azure-original.svg",
+      "/assets/img/icons/junit/junit-original.svg",
+            "/assets/img/icons/jupyter/jupyter-original.svg",
 
-      // "/img/icons/jacoco/jacoco.png",                // custom
-      "/img/icons/jira/jira-original.svg",
-      "/img/icons/postman/postman-original.svg",
-            "/img/icons/wordpress/wordpress-original.svg",
+      // "/assets/img/icons/jacoco/jacoco.png",                // custom
+      "/assets/img/icons/jira/jira-original.svg",
+      "/assets/img/icons/postman/postman-original.svg",
+            "/assets/img/icons/wordpress/wordpress-original.svg",
 
-      "/img/icons/kibana/kibana-original.svg",                  
-      // "/img/icons/powerbi/powerbi.png"               // custom
+      "/assets/img/icons/kibana/kibana-original.svg",                  
+      // "/assets/img/icons/powerbi/powerbi.png"               // custom
     ]
   }
 ];
@@ -135,7 +135,7 @@ skills: Skill[] = [
   testimonials: { title: string; icon: string; subTitle: string; text: string; }[] = [
     {
       title: 'Adrien Makarowicz',
-      icon: "img/profile-icon.png",
+      icon: "assets/img/profile-icon.png",
       subTitle: 'Responsable informatique - Wedeal',
       text: 'J\'ai eu le plaisir d\'accompagner Wafae tout au long de son parcours au sein de WeDeal, à la fois en tant que tuteur pendant deux ans et comme responsable hiérarchique. Cela m’a permis d’observer de près sa progression et son évolution constante en tant que développeuse web fullstack et gestionnaire de projet.Au fil des années, Wafae a démontré une forte montée en compétence sur plusieurs fronts :Maîtrise technique : Elle a développé une compréhension approfondie de divers langages informatiques et des outils spécifiques à notre entreprise, renforçant ainsi son efficacité dans les projets techniques.Autonomie et analyse métier : Sa capacité à analyser nos activités, prendre du recul et proposer des solutions pertinentes a été un atout précieux pour notre organisation.Expertise en TMA : Sa rigueur et sa finesse dans la Tierce Maintenance Applicative ont permis d’assurer une continuité et une qualité de service irréprochables.Gestion de projet : Wafae s’est distinguée par sa proactivité dans le cadrage des besoins, sa participation active aux rituels SCRUM, et sa structuration rigoureuse des projets dans des outils tels que Jira et Notion.Au-delà de ses compétences techniques et organisationnelles, Wafae a su instaurer un climat de confiance et de collaboration grâce à son professionnalisme et son écoute attentive, tant au sein du service informatique que dans ses interactions avec d’autres départements.Je recommande vivement Wafae, non seulement pour ses compétences techniques et sa rigueur, mais également pour sa capacité à gérer efficacement des projets et à collaborer avec divers interlocuteurs. Elle sera un atout indéniable pour toute organisation.'
     }
@@ -152,9 +152,9 @@ skills: Skill[] = [
     ];
 
     const sources = [
-      { src: 'img/profile/p1.png', alt: 'p1' },
-      { src: 'img/profile/p2.png', alt: 'p2' },
-      { src: 'img/profile/p3.png', alt: 'p3' }
+      { src: '/assets/img/profile/p1.png', alt: 'p1' },
+      { src: '/assets/img/profile/p2.png', alt: 'p2' },
+      { src: '/assets/img/profile/p3.png', alt: 'p3' }
     ];
 
     // Mélanger aléatoirement les positions
--- myportfolio/src/app/sections/portfolio/portfolio.component.ts
+++ myportfolio/src/app/sections/portfolio/portfolio.component.ts
@@ -23,23 +23,21 @@ export class PortfolioComponent {
   projects: Project[] = [
     {
       title: 'CV digital',
-      img: 'img/portfolio/portfolio.png',
+      img: '/assets/img/portfolio/portfoliocv.png',
       desc: `Ce projet est une version digitalisée de mon CV, présenté sous forme de portfolio interactif,
 développé avec Angular et Tailwind CSS. Il met en avant mes compétences techniques, mes projets et
 permet aux visiteurs de me contacter facilement ou de réserver un rendez-vous.`,
-      codeUrl: 'heheh',
-      demoUrl: "bbbb"
+      codeUrl: 'https://github.com/welmansouri/MyPortfolio',
     },
     {
-      title: 'Dashboard',
-      img: 'assets/img/portfolio/dashboard.png',
-      desc: 'Temps réel • Charts'
-    },
-    {
-      title: 'Réservation',
-      img: 'assets/img/portfolio/booking.png',
-      desc: 'Flutter • Stripe'
-    }
+      title: 'Coffee Connect',
+      img: '/assets/img/portfolio/coffe-connect.png',
+      desc: `Coffee-Connectest une application mobile innovante qui permet de gérer et personnaliser la distribution automatique de café directement depuis son smartphone. L’utilisateur peut choisir la taille, configurer ses préférences, payer et récupérer sa boisson en toute simplicité.
+Pensée pour l’avenir, la plateforme ne se limite pas seulement au café : elle ouvre la voie à la gestion connectée de tout type de distributeur automatique (snacks, boissons, produits divers), offrant une expérience fluide, moderne et 100 % mobile.`,
+      codeUrl: 'https://github.com/welmansouri/CoffeeConnect',
+      demoUrl:'https://appetize.io/app/b_w7uqkacrhgf4juuoh2ua6r2j3e',
+
+}
   ];
 
  isNonEmptyUrl(val: unknown): val is string {
